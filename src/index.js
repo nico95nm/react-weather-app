2541b34f529d0135764f781a0b90d25864a2cd66
LazyMan95 <nico95.nm@gmail.com>
2024-03-26 18:10:19 +0100

    Finalizing React Weather Project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './index.css';
-import React from 'react';
+import 'semantic-ui-css/semantic.min.css';
+import React, { useEffect, useState, UseState } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
